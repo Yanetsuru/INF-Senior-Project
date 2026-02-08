38e13f502b5259d5b80f646e87c732046458049e
Yanetsuru <yny220@aubg.edu>
2026-02-08 23:17:26 +0200

    Fixing submodule

---
+++ INF Senior Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INF_Senior_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
